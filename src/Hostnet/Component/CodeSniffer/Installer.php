acb50380792904c3ba9f3930db9e47418db7c35e
Stefan Lenselink <slenselink@hostnet.nl>
2015-04-02 17:15:26 +0200

    test if this works

---
+++ src/Hostnet/Component/CodeSniffer/Installer.php
@@ -0,0 +1,15 @@
+<?php
+namespace Hostnet\Component\CodeSniffer;
+use Composer\IO\IOInterface;
+use Composer\Composer;
+use Composer\Plugin\PluginInterface;
+
+class Installer implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Auto-generated method stub
+echo "Yo Ho!\n";
+    }
+
+}
