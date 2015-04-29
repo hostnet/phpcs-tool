bc1b00b7d77d51276748c26f499403479df955bb
Maarten Meijer <mmeijer@hostnet.nl>
2015-04-29 09:25:15 +0200

    Fixing Codestyle using PHPCBF

--- test/Hostnet/Tests/AbstractPHPCSBridge.php
+++ test/Hostnet/Tests/AbstractPHPCSBridge.php
@@ -17,15 +17,15 @@ abstract class Hostnet_Tests_AbstractPHPCSBridge extends AbstractSniffUnitTest
      */
     protected function setUp()
     {
-        $GLOBALS['PHP_CODESNIFFER_CONFIG_DATA'] = [
+        $GLOBALS['PHP_CODESNIFFER_CONFIG_DATA']                     = [
             'showSources' => true,
             'colors' => true,
             'default_standard' => 'Hostnet',
             'installed_paths' => __DIR__ . '/../../../src/'
         ];
         $GLOBALS['PHP_CODESNIFFER_STANDARD_DIRS'][get_class($this)] = __DIR__ . '/../..';
-        $GLOBALS['PHP_CODESNIFFER_SNIFF_CODES']   = [];
-        $GLOBALS['PHP_CODESNIFFER_FIXABLE_CODES'] = [];
+        $GLOBALS['PHP_CODESNIFFER_SNIFF_CODES']                     = [];
+        $GLOBALS['PHP_CODESNIFFER_FIXABLE_CODES']                   = [];
         parent::setUp();
     }
 
