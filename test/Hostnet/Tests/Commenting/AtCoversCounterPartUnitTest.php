d95469aa545a5af3f2b4aea058c7e87a0bce2429
Rick Prent <rprent@hostnet.nl>
2018-04-24 17:01:01 +0200

    Added a sniff to cover counter parts.

---
+++ test/Hostnet/Tests/Commenting/AtCoversCounterPartUnitTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @copyright 2016-2018 Hostnet B.V.
+ */
+declare(strict_types=1);
+
+namespace Hostnet\Tests\Commenting;
+
+use Hostnet\Tests\AbstractSniffUnitTest;
+
+/**
+ * @covers \Hostnet\Sniffs\Commenting\AtCoversCounterPartSniff
+ */
+class AtCoversCounterPartUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getErrorList($filename = '')
+    {
+        $list = [
+            'AtCoversCounterPartUnitTest.php.0.inc' => [],
+            'AtCoversCounterPartUnitTest.php.1.inc' => [],
+            'AtCoversCounterPartUnitTest.php.2.inc' => [
+                4  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.3.inc' => [
+                4  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.4.inc' => [
+                7  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.5.inc' => [
+                7  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.6.inc' => [
+                8  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.7.inc' => [
+                6  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.8.inc' => [
+                9  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.9.inc' => [
+                7  => 1,
+            ],
+            'AtCoversCounterPartUnitTest.php.10.inc' => [
+                7  => 1,
+            ],
+        ];
+
+        if (! isset($list[$filename])) {
+            return [];
+        }
+        return $list[$filename];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
